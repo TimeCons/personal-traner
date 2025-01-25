3456e861a8467f3ec21f35491a5e5a8a44ca37d5
Manuel Sansone <manuel.sansone@time.consulting.it>
2025-01-25 16:20:52 +0100

    modificate tariffe coach online

--- src/screen/PersonalTrainer/PersonalTrainer.tsx
+++ src/screen/PersonalTrainer/PersonalTrainer.tsx
@@ -157,18 +157,6 @@ export default function PersonalTrainer() {
             handleOpen("pacchetti lezioni - " + personalTrainer[2].title);
           }}
         />
-        <PricingTab
-          rowed
-          title={personalTrainer[3].title}
-          description={personalTrainer[3].description}
-          price={personalTrainer[3].price}
-          voices={personalTrainer[3].voices}
-          nolimit
-          hideMonthly
-          onClick={() => {
-            handleOpen("pacchetti lezioni - " + personalTrainer[3].title);
-          }}
-        />
       </div>
     </div>
   );
